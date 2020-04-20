0f49432e356344b0b5f53808035e1285fff2e995
ko <kyokucho1989@gmail.com>
2020-04-20 21:26:00 +0900

    編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,13 @@ class UsersController < ApplicationController
         # Strong Parameter の記述
         User.create(user_params)
     end
+    def edit
+        @user = User.find(params[:id])
+    end
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+    end
     def show
         @user = User.find(params[:id])
     end
