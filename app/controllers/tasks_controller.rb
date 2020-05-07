8daf866c80e0f39912722534364bbc59d99dbe4f
mei <oniku.1200tax@gmail.com>
2020-05-07 22:09:38 +0900

    7.destroyの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,6 +24,11 @@ def update
     task.update(task_params)
 end
 
+def destroy
+    task = Task.find(params[:id])
+    task.destroy
+end
+
 private
 def task_params
     params.require(:task).permit(:title, :content)
